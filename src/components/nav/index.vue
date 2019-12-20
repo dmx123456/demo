c799987cef456269cb3ee47625d2cc3787c1322c
cyf <chenyinfeng2017@sina.com>
2019-12-20 13:43:33 +0800

    11

--- src/components/nav/index.vue
+++ src/components/nav/index.vue
@@ -3,7 +3,7 @@
     <el-radio-group v-model="isCollapse"
                     style="margin-bottom: 20px;">
       <el-radio-button :label="false">展开121391</el-radio-button>
-      <el-radio-button :label="true">收起123</el-radio-button>
+      <el-radio-button :label="true">收起记录卡</el-radio-button>
     </el-radio-group>
     <el-menu :default-active="this.$route.path"
              router

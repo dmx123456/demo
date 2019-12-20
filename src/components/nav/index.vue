6292bbe547faffb2c325e45c29ead3021fc04991
dmx123456 <1737276713@qq.com>
2019-12-20 13:58:33 +0800

    qwe

--- src/components/nav/index.vue
+++ src/components/nav/index.vue
@@ -3,7 +3,7 @@
     <el-radio-group v-model="isCollapse"
                     style="margin-bottom: 20px;">
       <el-radio-button :label="false">展开1213910</el-radio-button>
-      <el-radio-button :label="true">收起aaa</el-radio-button>
+      <el-radio-button :label="true">收起aa</el-radio-button>
     </el-radio-group>
     <el-menu :default-active="this.$route.path"
              router
